2ba69a56cd6dfcfe228d3166b2c2469a2430a356
Pavel <krobynetspavel@gmail.com>
2025-02-06 13:22:07 +0200

    product item - product list

--- src/app/components/main/Featured.tsx
+++ src/app/components/main/Featured.tsx
@@ -1,9 +1,9 @@
 import SingleItemCard from "./SingleItemCard";
 export default function Featured() {
   return (
-    <div className="flex flex-col my-16 fixed-w px-5">
-      <h4 className="font-medium text-3xl capitalize">Featured products</h4>
-      <div className="flex flex-row items-center justify-between gap-10">
+    <div className="flex flex-col my-16 fixed-w px-5 gap-7">
+      <h4 className="font-medium text-4xl capitalize">Featured products</h4>
+      <div className="flex flex-row items-center justify-between flex-wrap gap-4">
         <SingleItemCard />
         <SingleItemCard />
         <SingleItemCard />
--- src/app/components/main/SingleItemCard.tsx
+++ src/app/components/main/SingleItemCard.tsx
@@ -1,22 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function SingleItemCart() {
   return (
-    <div className="flex flex-col justify-center gap-1">
-      <Image
-        src={"/placeholder.jpg"}
-        alt="item-image"
-        width={500}
-        height={500}
-        className="w-[300px] h-[300px] object-cover rounded-md"
-      />
-      <div className="flex flex-row justify-between">
-        <p className="font-bold text-lg">item name</p>
-        <p className="font-bold text-lg">$40</p>
-      </div>
-      <p className="text-sm font-medium">
-        Lorem ipsum dolor sit amet consectetur
-      </p>
-      <button className="mt-2 py-1 w-24 border border-redish text-redish rounded-2xl">
+    <div className="flex flex-col justify-center flex-wrap w-full md:w-[45%] lg:w-[23%]">
+      <Link href="#" className="flex flex-col w-full gap-2">
+        <div className="relative  h-80">
+          <Image
+            src={"/placeholder.jpg"}
+            alt="item-image"
+            fill
+            sizes="25vw"
+            className="object-cover rounded-md absolute z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src={"/placeholder2.jpg"}
+            alt="item-image"
+            fill
+            sizes="25vw"
+            className="object-cover rounded-md absolute"
+          />
+        </div>
+        <div className="flex flex-row justify-between">
+          <p className="font-bold text-lg">item name</p>
+          <p className="font-bold text-lg">$40</p>
+        </div>
+        <p className="text-sm font-medium">
+          Lorem ipsum dolor sit amet consectetur
+        </p>
+      </Link>
+      <button className="mt-3 py-1 w-24 border border-redish text-redish rounded-2xl hover:bg-redish hover:text-white">
         Add to Cart
       </button>
     </div>
