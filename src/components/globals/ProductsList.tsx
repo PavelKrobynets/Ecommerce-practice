c1d209af9ea2f6301f0b9622ba2510782304fd51
Pavel <krobynetspavel@gmail.com>
2025-02-17 17:08:20 +0200

    change list to server component

--- src/components/globals/ProductsList.tsx
+++ src/components/globals/ProductsList.tsx
@@ -1,8 +1,6 @@
-"use client";
-import { useState, useEffect } from "react";
-import { wixClient } from "context/wixContext";
 import SingleItemCard from "./SingleItemCard";
 import { Suspense } from "react";
+import { wixClientServer } from "lib/wixClientServer";
 import { IProduct } from "../../types/type";
 
 interface IProps {
@@ -10,54 +8,39 @@ interface IProps {
   category: string;
 }
 
-export default function ProductsList({ title, category }: IProps) {
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [error, setError] = useState<string | null>(null);
+export default async function ProductsList({ title, category }: IProps) {
+  const wixClient = await wixClientServer();
+  try {
+    const { collection } = await wixClient.collections.getCollectionBySlug(
+      category
+    );
+    if (!collection?._id) {
+      return null;
+    }
+    const wixProductsList = await wixClient.products
+      .queryProducts()
+      .hasSome("collectionIds", [collection._id])
+      .descending("lastUpdated")
+      .find();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const { collection } = await wixClient.collections.getCollectionBySlug(
-          category
-        );
-
-        if (!collection?._id) {
-          return null;
-        }
-        const productsList = await wixClient.products
-          .queryProducts()
-          .hasSome("collectionIds", [collection._id])
-          .descending("lastUpdated")
-          .find();
-
-        if (!productsList.items.length) {
-          return null;
-        }
-
-        setProducts(productsList.items as IProduct[]);
-      } catch (err) {
-        setError("Failed to fetch products");
-        console.error(err);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
-  if (error) {
-    return <div>Error: {error}</div>;
+    if (!wixProductsList.items.length) {
+      return null;
+    }
+    const products = (await wixProductsList.items) as IProduct[];
+    return (
+      <section className="flex flex-col my-16 lg:my-5 fixed-w px-5 gap-7">
+        <h4 className="font-medium text-4xl capitalize">{title}</h4>
+        <div className="flex flex-row items-center justify-between flex-wrap gap-4">
+          <Suspense fallback={"Loading..."}>
+            {products.map((product) => (
+              <SingleItemCard key={product._id} {...product} />
+            ))}
+          </Suspense>
+        </div>
+      </section>
+    );
+  } catch (e) {
+    console.error(e);
+    return <div>Failed fetching products</div>;
   }
-
-  return (
-    <section className="flex flex-col my-16 lg:my-5 fixed-w px-5 gap-7">
-      <h4 className="font-medium text-4xl capitalize">{title}</h4>
-      <div className="flex flex-row items-center justify-between flex-wrap gap-4">
-        <Suspense fallback={"Loading..."}>
-          {products.map((product) => (
-            <SingleItemCard key={product._id} {...product} />
-          ))}
-        </Suspense>
-      </div>
-    </section>
-  );
 }
